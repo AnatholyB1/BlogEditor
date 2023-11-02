a6a1b8b243277a3d197947d9c12a10a0addf6642
AnatholyB1 <93599884+AnatholyB1@users.noreply.github.com>
2023-11-02 16:40:17 +0700

    db

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) 
 {
-  if(typeof window !== "undefined"){return null}
+  if(typeof window === "undefined"){return null}
   
   return (
     <>
